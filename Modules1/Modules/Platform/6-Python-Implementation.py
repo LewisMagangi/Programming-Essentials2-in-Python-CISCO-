8cac809990ce1b311827acd293c243d5cb45979e
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-11 11:45:48 +0000

    illustrating the use of python_implementation and python_version_tuple functions

---
+++ Modules1/Modules/Platform/6-Python-Implementation.py
@@ -0,0 +1,5 @@
+from platform import python_implementation
+
+""" The python_implementation() function returns a string denoting the Python implementation (expect CPython here, unless you decide to use any non-canonical Python branch)
+"""
+print(python_implementation())
---
+++ Modules1/Modules/Platform/7-Python-Version-Tuple-func.py
@@ -0,0 +1,4 @@
+from platform import python_version_tuple
+
+for atr in python_version_tuple():
+    print(atr)
