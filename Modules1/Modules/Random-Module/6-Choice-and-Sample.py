65de073e7721a8e801df137efc8486f3bea8d2bb
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-11 10:40:35 +0000

    commenting codes

--- Modules1/Modules/Random-Module/6-Choice-and-Sample.py
+++ Modules1/Modules/Random-Module/6-Choice-and-Sample.py
@@ -2,6 +2,8 @@ from random import choice, sample
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+"""The first choice function chooses a "random" element from the input sequence and returns it."""
+
 print(choice(my_list))
-print(sample(my_list, 5))
+print(sample(my_list, 5))    # builds a list (a sample) consisting of the elements_to_choose element "drawn" from the input sequence.
 print(sample(my_list, 10))
