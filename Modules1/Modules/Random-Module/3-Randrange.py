be0610e57e3014a94de2ee9d473aaac7e9637b13
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-11 08:22:46 +0000

    just wow

--- Modules1/Modules/Random-Module/3-Randrange.py
+++ Modules1/Modules/Random-Module/3-Randrange.py
@@ -1,5 +1,5 @@
 from random import randrange 
 
-print(randrange(7), end=' ')
-print(randrange(2, 9), end=' ')
-print(randrange(1,89, 5), end=' ')
+print(randrange(7), end='\n')
+print(randrange(2, 9), end='\n')
+print(randrange(1,89, 5), end='\n')
--- Modules1/Modules/Random-Module/5-Random-but-deterministic.py
+++ Modules1/Modules/Random-Module/5-Random-but-deterministic.py
@@ -1,4 +1,4 @@
 from random import randint
 
 for i in range(10):
-    print(randint(1, 10), end=',')
+    print(randint(1, 10), end='\n')
