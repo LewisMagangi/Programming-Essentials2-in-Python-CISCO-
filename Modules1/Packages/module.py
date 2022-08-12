d30cedfdc465978494d748200c5ff84e3e3c34c7
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-12 12:39:53 +0000

    module updated

--- Modules1/Packages/module.py
+++ Modules1/Packages/module.py
@@ -1,6 +1,32 @@
-counter = 0
+#!/usr/bin/env python3 
+
+""" module.py - an example of a Python module """
+
+"""To inform users that this is your variable, that they may read it, but that they should not modify it under any circumstances. You precede the variable's name with _ (one underscore) or __ (two underscores), but remember, it's only a convention. Your module's users may obey it or they may not."""
+
+__counter = 0
+
+
+def suml(the_list):
+    global __counter
+    __counter += 1
+    the_sum = 0
+    for element in the_list:
+        the_sum += element
+    return the_sum
+
+
+def prodl(the_list):
+    global __counter    
+    __counter += 1
+    prod = 1
+    for element in the_list:
+        prod *= element
+    return prod
+
 
 if __name__ == "__main__":
-    print("I prefer to be a module.")
-else:
-    print("I like to be a module.")
+    print("I prefer to be a module, but I can do some tests for you.")
+    my_list = [i+1 for i in range(5)]
+    print(suml(my_list) == 15)
+    print(prodl(my_list) == 120)
