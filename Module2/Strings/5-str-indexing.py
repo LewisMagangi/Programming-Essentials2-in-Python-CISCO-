69ff9d9d8a4ec3bcd19aa00fb0f01d86d284629c
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-24 12:23:53 +0000

    strings

---
+++ Module2/Strings/5-str-indexing.py
@@ -0,0 +1,8 @@
+# Indexing strings.
+
+the_string = 'lewis codes like a monster'
+
+for x in range(len(the_string)):
+    print(the_string[x], end=' ')
+
+print()
---
+++ Module2/Strings/6-str-iterating.py
@@ -0,0 +1,8 @@
+# Iterating through a string.
+
+the_string = 'lewis codes like a monster'
+
+for character in the_string:
+    print(character, end=' ')
+
+print()
