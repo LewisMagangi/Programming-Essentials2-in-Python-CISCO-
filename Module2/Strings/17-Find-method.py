5891cedefd772681447a0d210cab2cdba712ab87
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-24 13:51:01 +0000

    more finding

--- Module2/Strings/17-Find-method.py
+++ Module2/Strings/17-Find-method.py
@@ -1,3 +1,8 @@
 # Demonstrating the find() method:
 print("Eta".find("ta"))
 print("Eta".find("mma"))
+t = 'theta'
+print(t.find('eta'))
+print(t.find('et'))
+print(t.find('the'))
+print(t.find('ha'))
