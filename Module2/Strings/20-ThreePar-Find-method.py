5ebaf775c01d2900eaaf734735c1b5b78436747a
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-24 14:05:30 +0000

    commenting my code

--- Module2/Strings/20-ThreePar-Find-method.py
+++ Module2/Strings/20-ThreePar-Find-method.py
@@ -1,3 +1,4 @@
-# Demonstrating the three parameter find() method :
+""" Demonstrating the three parameter find() method :
+Note : a cannot be found within the given search boundaries in the second print() """
 print('kappa'.find('a', 1, 4))
 print('kappa'.find('a', 2, 4))
