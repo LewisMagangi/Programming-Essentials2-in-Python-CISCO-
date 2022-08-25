826d610566477afed61e31a6a2e75d1b51b994c4
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-25 13:29:38 +0000

    string comparison

---
+++ Module2/Strings/40-string-comparison.py
@@ -0,0 +1,9 @@
+# Test examples here.
+print('alpha' == 'alpha')
+print('alpha' != 'Alpha')
+
+"""When you compare two strings of different lengths and the shorter one is identical to the longer one's beginning, the longer string is considered greater."""
+print('alpha' < 'alphabet')
+
+"""String comparison is always case-sensitive (upper-case letters are taken as lesser than lower-case)."""
+print('beta' > 'Beta') 
