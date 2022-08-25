eebca24825fac62763754dc4b0f5f57e8675d3d6
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-25 10:25:45 +0000

    rfinding

---
+++ Module2/Strings/33-rfind.py
@@ -0,0 +1,5 @@
+# Demonstrating the lstrip() method:
+print("[" + "            I'm a fantastic kid working on my goals                  ".rstrip() + "]")
+random_string = 'this is good     '
+print("[" + random_string.rstrip() + "]")
+
