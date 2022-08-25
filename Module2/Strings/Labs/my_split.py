04fbb81c2440ff11d8321213f90c7f75ec2e9137
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-25 20:03:00 +0000

    partially done

--- Module2/Strings/Labs/my_split.py
+++ Module2/Strings/Labs/my_split.py
@@ -1,20 +1,18 @@
 def mysplit(strng):
     x = 0
-    my_list = list(strng)
-    for i in range(1, len(my_list) - 1):
-        if my_list[i] is not " ":
-            my_list[x] += my_list[i]
-        else :
-            x += 1
-           # del my_list[i]
-    return my_list
+    my_list = []
+    word = ''
+    for char in strng:
+        if char is not " ":
+            word += char
+        else:
+            my_list.append(word)
+            word = ''
+    my_list.append(word)
+    return my_list 
 
-"""
 print(mysplit("To be or not to be, that is the question"))
 print(mysplit("To be or not to be,that is the question"))
 print(mysplit("   "))
 print(mysplit(" abc "))
 print(mysplit(""))
-"""
-
-print(mysplit("To be or not to be, that is the question"))
