f9d5e83e5989f0ff398cc3ede6a6bb6e10991646
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-25 10:47:14 +0000

    stripping strings

---
+++ Module2/Strings/35-startswith.py
@@ -0,0 +1,3 @@
+# Demonstrating the startswith() method:
+print("omega has great nutrients".startswith("meg"))
+print("omega3 is an awesome drug".startswith("om"))
---
+++ Module2/Strings/36-strip.py
@@ -0,0 +1,8 @@
+# Demonstrating the strip() method:
+print("[" + "            I'm a fantastic kid working on my goals                  ".strip() + "]")
+random_string = '   this is good     '
+
+# All whitepsace are removed
+print(random_string.strip())
+
+
