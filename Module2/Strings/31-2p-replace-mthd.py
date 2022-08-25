5571498006626d76f4f32ad57ae2e01b7f28d4a3
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-25 10:07:53 +0000

    replacing strings

---
+++ Module2/Strings/31-2p-replace-mthd.py
@@ -0,0 +1,4 @@
+# Demonstrating the replace() method:
+print("www.netacad.com".replace("netacad.com", "pythoninstitute.org"))
+print("This is it!, we are done".replace("is", "are"))
+print("Apple juice so tasty".replace("juice", ""))
---
+++ Module2/Strings/32-3p-replace-mthd.py
@@ -0,0 +1,3 @@
+# Demonstrating the 3 parameter replace() method:
+print("This is it! we are done".replace("is", "are", 1))
+print("This is it! we are done".replace("is", "are", 2))
