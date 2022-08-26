78b9cd0f5ca5add723f77682ae2937af217a4dd7
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-26 13:57:34 +0000

    learning exceptions

--- Module2/Exceptions/0-The-anatomy-of-exceptions.py
+++ Module2/Exceptions/0-The-anatomy-of-exceptions.py
@@ -1,11 +1,10 @@
-"""
+
 try:
     y = 1 / 0
 except ArithmeticError:                #Note that replacing the exception's name with either Exception or BaseException won't change the program's behavior.
     print("Oooppsss...")
 
 print("THE END.")
-"""
 
 try:
     y = 1 / 0
