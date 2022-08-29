ac80aa9b7dfa64ae98fa4d0e9374c5f8977e25bb
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-29 14:38:18 +0000

    more

---
+++ Object-Oriented-Programming-Module3.py/Stacks/Three-Objects-from-the-stack.py
@@ -0,0 +1,25 @@
+'''
+ we've created three objects of the class Stack.
+'''
+class Stack:
+    def __init__(self):
+        self.__stack_list = []
+
+    def push(self, val):
+        self.__stack_list.append(val)
+
+    def pop(self):
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
+        return val
+
+
+little_stack = Stack()
+another_stack = Stack()
+funny_stack = Stack()
+
+little_stack.push(1)
+another_stack.push(little_stack.pop() + 1)
+funny_stack.push(another_stack.pop() - 2)
+
+print(funny_stack.pop())                 # Enter code here.
