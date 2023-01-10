dac74ba2dd590bfc1c91a43f450244bb73559f17
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-10 05:40:31 -0800

    two level inheritance example

---
+++ Object-Oriented-Programming-Module3.py/Inheritance/2.two-level-inheritance.py
@@ -0,0 +1,13 @@
+"""
+A very simple example of two-level inheritance is presented here:
+"""
+class Vehicle:
+    pass
+
+
+class LandVehicle(Vehicle):
+    pass
+
+
+class TrackedVehicle(LandVehicle):
+    pass
