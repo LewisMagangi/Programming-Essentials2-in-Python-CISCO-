dc1df9eefc84320124777b8c7f182fb48cbf233b
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-10 05:52:10 -0800

    issubclass function

--- Object-Oriented-Programming-Module3.py/Inheritance/2.two-level-inheritance.py
+++ Object-Oriented-Programming-Module3.py/Inheritance/2.two-level-inheritance.py
@@ -1,5 +1,8 @@
 """
 A very simple example of two-level inheritance is presented here:
+The Vehicle class is the superclass for both the LandVehicle and TrackedVehicle classes;
+The LandVehicle class is a subclass of Vehicle and a superclass of TrackedVehicle at the same time;
+The TrackedVehicle class is a subclass of both the Vehicle and LandVehicle classes.
 """
 class Vehicle:
     pass
